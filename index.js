33c2b7a079c31a0366638f42af5c68bc6da74892
jimmyurl <jimmy.james365@gmail.com>
2023-09-08 14:17:12 +0300

    recipes added

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const app = express();
 
+
+const recipes = [
+    {id: 1, name: 'pork loin'},
+    {id: 2, name: 'chili garlic sauce'},
+    {id: 3, name: 'olive oil'},
+    {id: 4, name: 'green onions'}
+]
+
 app.get('/', (req, res)=>{
 res.send('Ugali Kitimoto');
 });
 
 
 app.get('/api/recipes', (req, res) =>{
-    res.send(
-        [1, 2, 3]
-    );
+    res.send(recipes);
 });
 
 //Fetch specific id in the endpoint
 
 app.get('/api/recipes/:id', (req, res) =>{
-    res.send(req.params.id)
+    const recipe = recipes.find(r => r.id === parseInt(req.params.id))
+
+    if(!recipe) res.status(404).send("Recipe Ingredient is Not Found!!")
+    res.send(recipe)
+})
+//Query string parameter
+app.get('/api/posts/:year/:month',(req, res) =>{
+    res.send(req.query)
 })
 //PORT
 const port = process.env.PORT || 3000;
